71240a159a06b86d482017533cd28b4a72862e05
FHermanos Rodrigues <faabrs.rodrigues@gmail.com>
2023-05-06 16:55:51 -0300

    commit changes

---
+++ bd.py
@@ -0,0 +1,27 @@
+Pessoas = [
+    {
+        'id': 1,
+        'nome': 'Fabricio',
+        'idade': 19,
+    },
+    {
+        'id': 2,
+        'nome':'Juliana',
+        'idade': 26,
+    },
+    {
+        'id': 3,
+        'nome': 'Luan',
+        'idade': 22,
+    },
+    {
+        'id': 4,
+        'nome': 'Mariana',
+        'idade': 28,
+    },
+    {
+        'id': 5,
+        'nome': 'Carlos',
+        'idade': 38,
+    },
+]
---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, make_response, jsonify, request
+from bd import Pessoas # importa lista
+
+app = Flask(__name__) # define nome do app
+app.config['JSON_SORT_KEYS'] = False # desativa a ordenação em ordem alfabética
+
+@app.route('/pessoas', methods=['GET']) # decorator para o método GET
+def get_pessoas():
+    return make_response( # formatação
+        jsonify(
+            mensagem = 'Lista de Pessoas:',
+            dados = Pessoas
+        )
+    )
+
+@app.route('/pessoas', methods=['POST']) # decorator para o método POST
+def create_pessoa():
+    pessoa = request.json # recebe pessoa
+    Pessoas.append(pessoa) # anexa pessoa na lista
+    return make_response( # formatação
+        jsonify(
+            mensagem = 'Pessoa cadastrada com sucesso!',
+            pessoa = pessoa
+        )
+    )
+    
+
+app.run() # roda app
